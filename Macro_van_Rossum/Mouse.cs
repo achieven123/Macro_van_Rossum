ba29964a7e23872fb93c30e05e9c8d8896627a14
achieven123 <achieven123@naver.com>
2023-03-15 11:28:27 +0900

    초기 커밋.

---
+++ Macro_van_Rossum/Mouse.cs
@@ -0,0 +1,27 @@
+﻿namespace Macro_van_Rossum
+{
+    public partial class Mouse : Form
+    {
+        List<Tuple<int, string, int, int>> lists;
+
+        public Mouse(List<Tuple<int, string, int, int>> list)
+        {
+            InitializeComponent();
+
+            lists = list;
+
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void Mouse_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = Convert.ToInt32(Cursor.Position.X.ToString());
+            int y = Convert.ToInt32(Cursor.Position.Y.ToString());
+            int type = 0;
+
+            var click = (e.Button == MouseButtons.Left) ? "Left" : "Right";
+            lists.Add(new Tuple<int, string, int, int>(type, click, x, y));
+            this.Close();
+        }
+    }
+}
