1b4cc356026451cd11a7fffa79cbbcb121a7cf89
[achieven123] <achieven123@naver.com>
2023-04-23 10:43:38 +0900

    Update

--- Data_Structure/linked_list/linked_list.cpp
+++ Data_Structure/linked_list/linked_list.cpp
@@ -2,14 +2,37 @@
 #include <stdlib.h>
 
 typedef int element;
-typedef struct LinkedListType {
+typedef struct LinkedListNode {
 	element data;
-	LinkedListType* link;
-} LinkedListType;
+	LinkedListNode* link;
+} LinkedListNode;
 
+LinkedListNode* InsertFirstNode(LinkedListNode* head, element value) {
+	LinkedListNode* p = (LinkedListNode*)malloc(sizeof(LinkedListNode));
+
+	p->data = value;
+	p->link = head;
+	head = p;
+
+	return head;
+}
+
+LinkedListNode* InsertNode(LinkedListNode* head, LinkedListNode* pre, element value) {
+	LinkedListNode* p = (LinkedListNode*)malloc(sizeof(LinkedListNode));
+
+	p->data = value;
+	p->link = pre->link;
+	pre->link = p;
+
+	return head;
+}
+
+LinkedListNode* DeleteNode
 
 int main() {
-	
+	LinkedListNode* head = NULL;
+
+
 
 	return 0;
 }
