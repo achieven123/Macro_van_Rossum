8cfb1a29141bafbdbef6f59e21f4b4b4262c38be
[achieven123] <achieven123@naver.com>
2023-04-23 10:12:36 +0900

    Update

---
+++ Data_Structure/linked_list/linked_list.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+typedef struct LinkedListType {
+	element data;
+	LinkedListType* link;
+} LinkedListType;
+
+
+int main() {
+	
+
+	return 0;
+}
