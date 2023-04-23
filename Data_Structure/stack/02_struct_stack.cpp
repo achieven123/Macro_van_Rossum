26083877bd0436824d73fb12c2e9136ca730720b
achieven123 <achieven123@naver.com>
2023-04-23 22:35:31 +0900

    Update

---
+++ Data_Structure/stack/02_struct_stack.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 10
+
+typedef int element;
+
+typedef struct StackType {
+	element data[MAX];
+	int top;
+};
+
+void InitStack(StackType* s) {
+	s->top
+}
+
+int IsFull() {
+
+}
+
+int main() {
+
+
+	return 0;
+}
