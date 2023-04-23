03ad733931cb608f98ca4cb0a50078a13f642217
[achieven123] <achieven123@naver.com>
2023-04-23 11:28:40 +0900

    Update

--- Data_Structure/linked_list/linked_list.cpp
+++ Data_Structure/linked_list/linked_list.cpp
@@ -27,12 +27,65 @@ LinkedListNode* InsertNode(LinkedListNode* head, LinkedListNode* pre, element va
 	return head;
 }
 
-LinkedListNode* DeleteNode
+LinkedListNode* DeleteFirstNode(LinkedListNode* head) {
+	LinkedListNode* removed;
+
+	if (head == NULL) return NULL;
+
+	removed = head;
+	head = removed->link;
+	free(removed);
+
+	return head;
+}
+
+LinkedListNode* DeleteNode(LinkedListNode* head, LinkedListNode* pre) {
+	LinkedListNode* removed;
+
+	removed = pre->link;
+	pre->link = removed->link;
+	free(removed);
+	return head;
+}
+
+void PrintList(LinkedListNode* head) {
+	/*for (LinkedListNode* p = head; p != NULL; p = p->link) {
+		printf("%d->", p->data);
+	} printf("NULL\n");*/
+
+	LinkedListNode* p = head;
+
+	while (p != NULL) {
+		printf("%d->", p->data);
+		p = p->link;
+	} printf("NULL\n");
+}
+
+LinkedListNode* ConnectList(LinkedListNode* head1, LinkedListNode* head2) {
+	LinkedListNode* p = head1;
+
+	while (p->link != NULL) {
+		p = p->link;
+	}
+
+	p->link = head2;
+	return head1;
+}
+
+
 
 int main() {
-	LinkedListNode* head = NULL;
+	LinkedListNode* head1 = NULL;
+	LinkedListNode* head2 = NULL;
+	
+	for (int i = 0; i < 5; i++) {
+		head1 = InsertFirstNode(head1, i);
+		head2 = InsertFirstNode(head2, i);
+	}
 
+	head1 = ConnectList(head1, head2);
 
+	PrintList(head1);
 
 	return 0;
 }
