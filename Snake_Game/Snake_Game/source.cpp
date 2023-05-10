d7e9db37357ebbd3034e829e35511af70fd0ef46
achieven123 <achieven123@naver.com>
2023-05-10 14:42:48 +0900

    UPdate

--- Snake_Game/Snake_Game/source.cpp
+++ Snake_Game/Snake_Game/source.cpp
@@ -1,11 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <windows.h>
 
-void title() {
-	system("mode con cols=가로 lines=세로 | title 창 제목");
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
+#define PAUSE 112
+#define ESC 27
 
+#define MAP_X 3
+#define MAP_Y 2
+#define MAP_WIDTH 30
+#define MAP_HEIGHT 20
+
+void gotoxy(int x, int y, char* s) {
+	COORD pos = { 2 * x,y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+	printf("%s", s);
+}
+void gotoxy(int x, int y, const char* s) {
+	COORD pos = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+	printf("%s", s);
+}
+
+void title() {
+	system("title Snake Game");
+
+	printf("####### ###    ##  #####  ##   ## #######\n");
+	printf("##      ####   ## ##   ## ##  ##  ##\n");
+	printf("####### ## ##  ## ####### #####   #####\n");
+	printf("     ## ##  ## ## ##   ## ##  ##  ##\n");
+	printf("####### ##   #### ##   ## ##   ## #######\n\n");
+
+	printf(" ######   #####  ###    ### #######\n");
+	printf("##       ##   ## ####  #### ##\n");
+	printf("##   ### ####### ## #### ## #####\n");
+	printf("##    ## ##   ## ##  ##  ## ##\n");
+	printf(" ######  ##   ## ##      ## #######\n");
+}
+
+
+void draw_map(void) { //맵 테두리 그리는 함수 
+	for (int i = 0; i < MAP_WIDTH; i++) {
+		gotoxy(MAP_X + i, MAP_Y, "■");
+	}
+	for (int i = MAP_Y + 1; i < MAP_HEIGHT - 1; i++) {
+		gotoxy(MAP_X, i, "■");
+		gotoxy(MAP_X + MAP_WIDTH - 1, i, "■");
+	}
+	for (int i = 0; i < MAP_WIDTH; i++) {
+		gotoxy(MAP_X + i, MAP_Y + MAP_HEIGHT - 1, "■");
+	}
 }
 
 int main() {
+	title();
 
+
+	//draw_map();
 }
