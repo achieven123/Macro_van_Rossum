acbae8e9e48e3aa8a231206c472bd79e1a8f0e08
achieven123 <achieven123@naver.com>
2023-05-12 03:45:22 +0900

    Update soruce.cpp

--- Snake_Game/Snake_Game/soruce.cpp
+++ Snake_Game/Snake_Game/soruce.cpp
@@ -2,39 +2,12 @@
 #include <Windows.h>
 #include <conio.h>
 
-#define GAP_WIDTH 4
-#define GAP_HEIGHT 2
+#define GAP_WIDTH 6
+#define GAP_HEIGHT 6
 #define MAP_WIDTH 25
 #define MAP_HEIGHT 25
 
 int map[MAP_HEIGHT][MAP_WIDTH * 2] = { 0 };
-/*{
-	{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
-	{ 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 },
-	{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
-	{ 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 },
-	{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
-	{ 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 },
-	{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
-	{ 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 },
-	{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
-	{ 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 },
-	{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
-	{ 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 },
-	{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
-	{ 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 },
-	{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
-	{ 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 },
-	{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
-	{ 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 },
-	{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
-	{ 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 },
-	{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
-	{ 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 },
-	{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
-	{ 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 },
-	{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
-};*/
 
 enum {
 	BLACK,
@@ -64,12 +37,6 @@ void init();
 void draw_map();
 
 int main() {
-	getch();
-
-	//for (int i = 0; i < 65; i++)
-	//{
-	//	printf("%d ", map[0][i]);
-	//}
 	init();
 	draw_map();
 
@@ -82,56 +49,77 @@ void gotoxy(int x, int y) {
 }
 
 void init() {
+	CONSOLE_CURSOR_INFO cursorInfo = { 0, };
+	cursorInfo.bVisible = FALSE;
+	cursorInfo.dwSize = 1;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+
+	system("cls");
+	printf("Set the console window to full screen and press any key...");
+	_getch();
+	system("cls");
+
 	for (int y = 0; y < MAP_HEIGHT; y++) {
-		for (int x = 0; x < MAP_WIDTH; x ++) {
-			if ((x + y) % 2 != 0) { map[y][x * 2] = 1; map[y][x * 2 + 1] = 1; }
+		for (int x = 0; x < MAP_WIDTH; x++) {
+			if ((x + y) % 2 != 0) {
+				map[y][x * 2] = 1;
+				map[y][x * 2 + 1] = 1;
+			}
 		}
 	}
 }
 
 void draw_map() {
-	SetColor(GRAY, WHITE);
+
+	for (int i = 0; i < MAP_WIDTH * 2 + 4; i++) {
+		SetColor(D_GRAY, WHITE);
+		gotoxy(GAP_WIDTH - 2 + i, GAP_HEIGHT - 4);
+		printf(" ");
+		gotoxy(GAP_WIDTH - 2 + i, GAP_HEIGHT - 3);
+		printf("@");
+		gotoxy(GAP_WIDTH - 2 + i, GAP_HEIGHT - 2);
+		printf(" ");
+		
+		SetColor(GRAY, WHITE);
+		gotoxy(GAP_WIDTH - 2 + i, GAP_HEIGHT - 1);
+		printf(" ");
+		gotoxy(GAP_WIDTH - 2 + i, GAP_HEIGHT + MAP_HEIGHT);
+		printf(" ");
+	}
+
+	for (int i = 0; i < MAP_HEIGHT; i++) {
+		gotoxy(GAP_WIDTH - 2, GAP_HEIGHT + i);
+		printf("  ");
+		gotoxy(GAP_WIDTH + MAP_WIDTH * 2, GAP_HEIGHT + i);
+		printf("  ");
+	}
 
 	for (int y = 0; y < MAP_HEIGHT; y++) {
 		for (int x = 0; x < MAP_WIDTH * 2; x++) {
-
-
 			gotoxy(GAP_WIDTH + x, GAP_HEIGHT + y);
 			if (map[y][x] == 0) { SetColor(GREEN, WHITE); printf(" "); }
 			if (map[y][x] == 1) { SetColor(D_GREEN, WHITE); printf(" "); }
-
-			//if (y == 0 || y == MAP_HEIGHT + 1) { SetColor(D_GRAY, WHITE); printf("%2d", x); }
-			//else if (x == 0 || x == MAP_WIDTH + 1) { SetColor(D_GRAY, WHITE); printf("%2d", y); }
-			//else { SetColor(GREEN, WHITE); printf("  "); }
 		}
 	}
 
-	//for (int i = 0; i < MAP_HEIGHT + 1; i++) {
-	//	gotoxy(GAP_WIDTH, GAP_HEIGHT + i);
-	//	printf("%2d", i);
+	gotoxy(GAP_WIDTH + 10, GAP_HEIGHT + 10);
+	SetColor(RED, WHITE); printf(" ");
+	gotoxy(GAP_WIDTH + 11, GAP_HEIGHT + 10);
+	printf(" ");
+	gotoxy(GAP_WIDTH + 12, GAP_HEIGHT + 10);
+	printf(" ");
+	gotoxy(GAP_WIDTH + 13, GAP_HEIGHT + 10);
+	printf(" ");
+	gotoxy(GAP_WIDTH + 14, GAP_HEIGHT + 10);
+	printf(" ");
+	gotoxy(GAP_WIDTH + 15, GAP_HEIGHT + 10);
+	printf(" ");
+	gotoxy(GAP_WIDTH + 16, GAP_HEIGHT + 10);
+	printf(" ");
+	gotoxy(GAP_WIDTH + 17, GAP_HEIGHT + 10);
+	printf(" ");
 
-	//	//for (int j = 2; j < MAP_WIDTH + 1; j += 4) {
-	//	//	SetColor(GREEN, WHITE);
-	//	//	gotoxy(GAP_WIDTH + j, GAP_HEIGHT + i + 1);
-	//	//	printf("  ");
-	//	//}
-
-	//	//for (int j = 4; j < MAP_WIDTH + 1; j += 4) {
-	//	//	SetColor(D_GREEN, WHITE);
-	//	//	gotoxy(GAP_WIDTH + j, GAP_HEIGHT + i + 1);
-	//	//	printf("%2d", j);
-	//	//}
-
-	//	SetColor(GRAY, WHITE);
-
-	//	gotoxy(GAP_WIDTH + MAP_WIDTH + 2, GAP_HEIGHT + i);
-	//	printf("  ");
-	//}
-
-	//for (int i = 0; i < MAP_WIDTH + 4; i ++) {
-	//	gotoxy(GAP_WIDTH + i, GAP_HEIGHT + MAP_HEIGHT + 1);
-	//	printf(" ");
-	//}
+	printf("\n");
 	printf("\n");
 	SetColor(BLACK, WHITE);
 }
