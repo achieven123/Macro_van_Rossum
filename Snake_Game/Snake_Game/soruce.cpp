3244a406f88e78b2783b91dc5fe3d98f9e9540b3
achieven123 <achieven123@naver.com>
2023-05-15 14:08:21 +0900

    Update

--- Snake_Game/Snake_Game/soruce.cpp
+++ Snake_Game/Snake_Game/soruce.cpp
@@ -2,48 +2,36 @@
 #include <stdlib.h>
 #include <Windows.h>
 #include <conio.h>
-#pragma region ÀÔ·Â
-#define W 119
-#define S 115
-#define A 97
-#define D 100
+
+#pragma region Key Input
 
 #define UP 72
 #define DOWN 80
 #define LEFT 75
 #define RIGHT 77
 
+#define W 119
+#define S 115
+#define A 97
+#define D 100
+
 #define ESC 27
 #define ENTER 13
+
 #pragma endregion
-#define THICKNESS 3
-#define GAP_WIDTH 6
-#define GAP_HEIGHT 6
+
+#pragma region Draw Map
+
+#define GAP_X 3
+#define GAP_Y 3
 #define MAP_WIDTH 25
 #define MAP_HEIGHT 25
+
+#pragma endregion 
 
 #define SNAKE_LENGTH 5
 #define START_POINT_X 12
 #define START_POINT_Y 12
-
-typedef int element;
-
-typedef struct LinkedListNode {
-	element location_x;
-	element location_y;
-	LinkedListNode* left_link;
-	LinkedListNode* right_link;
-} LinkedListNode;
-//
-//LinkedListNode* InsertFirstNode(LinkedListNode* head, element value) {
-//	LinkedListNode* p = (LinkedListNode*)malloc(sizeof(LinkedListNode));
-//
-//	p->data = value;
-//	p->link = head;
-//	head = p;
-//
-//	return head;
-//}
 
 int map[MAP_HEIGHT][MAP_WIDTH] = { 0 };
 int score = 0;
@@ -83,17 +71,6 @@ void draw_map();
 void draw_info();
 
 void game_start();
-void init_snake(LinkedListNode* head, LinkedListNode* tail);
-void insert_front(LinkedListNode* head, LinkedListNode* tail, int direction);
-void delete_node(LinkedListNode* head, LinkedListNode* removed);
-
-void print_list(LinkedListNode* head, LinkedListNode* tail) {
-	LinkedListNode* p;
-
-	for (p = head->right_link; p != NULL; p = p->right_link)
-		printf("<-%d %d->", p->location_x, p->location_y);
-	printf("\n");
-}
 
 int main() {
 	init();
@@ -102,7 +79,6 @@ int main() {
 	draw_info();
 
 	game_start();
-
 	return 0;
 }
 
@@ -127,14 +103,14 @@ int input_key() {
 
 void set_block(int x, int y, int color) {
 	set_color(color, WHITE);
-	gotoxy(GAP_WIDTH + x * 2, GAP_HEIGHT + y);
+	gotoxy(GAP_X + x * 2, GAP_Y + y);
 	printf("  ");
 }
 
 void delete_block(int x, int y) {
-	if (map[GAP_WIDTH + x][GAP_HEIGHT + y] == 0) set_color(GREEN, WHITE);
+	if (map[GAP_X + x][GAP_Y + y] == 0) set_color(GREEN, WHITE);
 	else set_color(D_GREEN, WHITE);
-	gotoxy(GAP_WIDTH + x * 2, GAP_HEIGHT + y);
+	gotoxy(GAP_X + x * 2, GAP_Y + y);
 	printf("  ");
 }
 
@@ -163,24 +139,24 @@ void init() {
 void draw_window() {
 	for (int i = 0; i < MAP_WIDTH * 2 + 4; i++) {
 		set_color(D_GRAY, WHITE);
-		gotoxy(GAP_WIDTH - THICKNESS + i, GAP_HEIGHT - 4);
+		gotoxy(GAP_X - THICKNESS + i, GAP_Y - 4);
 		printf(" ");
-		gotoxy(GAP_WIDTH - THICKNESS + i, GAP_HEIGHT - 3);
+		gotoxy(GAP_X - THICKNESS + i, GAP_Y - 3);
 		printf(" ");
-		gotoxy(GAP_WIDTH - THICKNESS + i, GAP_HEIGHT - 2);
+		gotoxy(GAP_X - THICKNESS + i, GAP_Y - 2);
 		printf(" ");
 
 		set_color(GRAY, WHITE);
-		gotoxy(GAP_WIDTH - THICKNESS + i, GAP_HEIGHT - 1);
+		gotoxy(GAP_X - THICKNESS + i, GAP_Y - 1);
 		printf(" ");
-		gotoxy(GAP_WIDTH - THICKNESS + i, GAP_HEIGHT + MAP_HEIGHT);
+		gotoxy(GAP_X - THICKNESS + i, GAP_Y + MAP_HEIGHT);
 		printf(" ");
 	}
 
 	for (int i = 0; i < MAP_HEIGHT; i++) {
-		gotoxy(GAP_WIDTH - THICKNESS, GAP_HEIGHT + i);
+		gotoxy(GAP_X - THICKNESS, GAP_Y + i);
 		printf("  ");
-		gotoxy(GAP_WIDTH - THICKNESS + THICKNESS + MAP_WIDTH * 2, GAP_HEIGHT + i);
+		gotoxy(GAP_X - THICKNESS + THICKNESS + MAP_WIDTH * 2, GAP_Y + i);
 		printf("  ");
 	}
 }
@@ -188,11 +164,11 @@ void draw_window() {
 void draw_map() {
 	for (int y = 0; y < MAP_HEIGHT; y++) {
 		for (int x = 0; x < MAP_WIDTH; x++) {
-			gotoxy(GAP_WIDTH + x * 2, GAP_HEIGHT + y);
+			gotoxy(GAP_X + x * 2, GAP_Y + y);
 			if (map[y][x] == 0) { set_color(GREEN, WHITE); printf(" "); }
 			if (map[y][x] == 1) { set_color(D_GREEN, WHITE); printf(" "); }
 
-			gotoxy(GAP_WIDTH + x * 2 + 1, GAP_HEIGHT + y);
+			gotoxy(GAP_X + x * 2 + 1, GAP_Y + y);
 			if (map[y][x] == 0) { set_color(GREEN, WHITE); printf(" "); }
 			if (map[y][x] == 1) { set_color(D_GREEN, WHITE); printf(" "); }
 		}
@@ -201,105 +177,27 @@ void draw_map() {
 
 void draw_info() {
 	set_color(D_GRAY, WHITE);
-	gotoxy(GAP_WIDTH, GAP_HEIGHT - 3);
+	gotoxy(GAP_X, GAP_Y - 3);
 	printf("SCORE %d", score);
-	gotoxy(GAP_WIDTH + 35, GAP_HEIGHT - 3);
+	gotoxy(GAP_X + 35, GAP_Y - 3);
 	printf("20223070 ±è°æÈÆ");
 
 	int width = 60;
 	int height = 16;
 
 	set_color(BLACK, WHITE);
-	gotoxy(GAP_WIDTH + width + 5, GAP_HEIGHT + height);
+	gotoxy(GAP_X + width + 5, GAP_Y + height);
 	printf("SNAKE GAME");
 
-	gotoxy(GAP_WIDTH + width, GAP_HEIGHT + height + 3);
+	gotoxy(GAP_X + width, GAP_Y + height + 3);
 	printf("High Socre : %d", max_score);
 
-	gotoxy(GAP_WIDTH + width, GAP_HEIGHT + height + 5);
+	gotoxy(GAP_X + width, GAP_Y + height + 5);
 	printf("Move Up    : W or ¡è");
-	gotoxy(GAP_WIDTH + width, GAP_HEIGHT + height + 6);
+	gotoxy(GAP_X + width, GAP_Y + height + 6);
 	printf("Move Down  : S or ¡é");
-	gotoxy(GAP_WIDTH + width, GAP_HEIGHT + height + 7);
+	gotoxy(GAP_X + width, GAP_Y + height + 7);
 	printf("Move Left  : A or ¡ç");
-	gotoxy(GAP_WIDTH + width, GAP_HEIGHT + height + 8);
+	gotoxy(GAP_X + width, GAP_Y + height + 8);
 	printf("Move Right : D or ¡æ");
-}
-
-void game_start() {
-	LinkedListNode* head = (LinkedListNode*)malloc(sizeof(LinkedListNode));
-	LinkedListNode* tail = (LinkedListNode*)malloc(sizeof(LinkedListNode));
-	init_snake(head, tail);
-
-	int dir = RIGHT;
-	
-	while (true) {
-		if (kbhit()) {
-			int input = input_key();
-
-			if (input == ESC) break;
-
-			if (input == UP || input == DOWN || input == LEFT || input == RIGHT) {
-				if ((input != UP && dir == DOWN) || (input != DOWN && dir == UP) ||
-					(input != LEFT && dir == RIGHT) || (input != RIGHT && dir == LEFT)) {
-					dir = input;
-				}
-			}
-		}
-
-		gotoxy(0, 0);
-		print_list(head, tail);
-		//insert_front(head, tail, dir);
-		Sleep(speed);
-	}
-}
-
-void init_snake(LinkedListNode* head, LinkedListNode* tail) {
-	head->location_x = START_POINT_X;
-	head->location_y = START_POINT_Y;
-	tail->location_x = START_POINT_X - 1;
-	tail->location_y = START_POINT_Y;
-
-	head->left_link = NULL;
-	head->right_link = tail;
-	tail->left_link = head;
-	tail->right_link = NULL;
-
-	set_block(head->location_x, head->location_y, BLUE);
-}
-
-void insert_front(LinkedListNode* head, LinkedListNode* tail, int direction) {
-	LinkedListNode* new_node = (LinkedListNode*)malloc(sizeof(LinkedListNode));
-
-	new_node->location_x = head->location_x;
-	new_node->location_y = head->location_y;
-
-	new_node->right_link = head;
-	head->left_link = new_node;
-	head = new_node;
-
-	/*new_node->left_link = head;
-	new_node->right_link = head->right_link;
-	head->right_link->left_link = new_node;
-	head->right_link = new_node;*/
-
-	if (direction == UP) head->location_y--;
-	if (direction == DOWN) head->location_y++;
-	if (direction == LEFT) head->location_x--;
-	if (direction == RIGHT) head->location_x++;
-
-	set_block(head->location_x, head->location_y, BLUE);
-
-	//delete_node(head, tail->left_link);
-}
-
-void delete_node(LinkedListNode* head, LinkedListNode* removed) {
-	//if (removed == head) return;
-
-	//delete_block(removed->location_x, removed->location_y);
-
-	removed->right_link->left_link = removed->left_link;
-	removed->left_link->right_link = removed->right_link;
-
-	free(removed);
 }
--- Snake_Game/Snake_Game/test.cpp
+++ Snake_Game/Snake_Game/test.cpp
@@ -20,10 +20,100 @@ typedef struct LinkedListNode {
 //	return head;
 //}
 
+void init_snake(LinkedListNode* head, LinkedListNode* tail);
+void insert_front(LinkedListNode* head, LinkedListNode* tail, int direction);
+void delete_node(LinkedListNode* head, LinkedListNode* removed);
+
+void print_list(LinkedListNode* head, LinkedListNode* tail) {
+	LinkedListNode* p;
+
+	for (p = head->right_link; p != NULL; p = p->right_link)
+		printf("<-%d %d->", p->location_x, p->location_y);
+	printf("\n");
+}
+
 
 
 int main() {
 
 
 	return 0;
+}
+
+void game_start() {
+	LinkedListNode* head = (LinkedListNode*)malloc(sizeof(LinkedListNode));
+	LinkedListNode* tail = (LinkedListNode*)malloc(sizeof(LinkedListNode));
+	init_snake(head, tail);
+
+	int dir = RIGHT;
+
+	while (true) {
+		if (kbhit()) {
+			int input = input_key();
+
+			if (input == ESC) break;
+
+			if (input == UP || input == DOWN || input == LEFT || input == RIGHT) {
+				if ((input != UP && dir == DOWN) || (input != DOWN && dir == UP) ||
+					(input != LEFT && dir == RIGHT) || (input != RIGHT && dir == LEFT)) {
+					dir = input;
+				}
+			}
+		}
+
+		gotoxy(0, 0);
+		print_list(head, tail);
+		//insert_front(head, tail, dir);
+		Sleep(speed);
+	}
+}
+
+void init_snake(LinkedListNode* head, LinkedListNode* tail) {
+	head->location_x = START_POINT_X;
+	head->location_y = START_POINT_Y;
+	tail->location_x = START_POINT_X - 1;
+	tail->location_y = START_POINT_Y;
+
+	head->left_link = NULL;
+	head->right_link = tail;
+	tail->left_link = head;
+	tail->right_link = NULL;
+
+	set_block(head->location_x, head->location_y, BLUE);
+}
+
+void insert_front(LinkedListNode* head, LinkedListNode* tail, int direction) {
+	LinkedListNode* new_node = (LinkedListNode*)malloc(sizeof(LinkedListNode));
+
+	new_node->location_x = head->location_x;
+	new_node->location_y = head->location_y;
+
+	new_node->right_link = head;
+	head->left_link = new_node;
+	head = new_node;
+
+	/*new_node->left_link = head;
+	new_node->right_link = head->right_link;
+	head->right_link->left_link = new_node;
+	head->right_link = new_node;*/
+
+	if (direction == UP) head->location_y--;
+	if (direction == DOWN) head->location_y++;
+	if (direction == LEFT) head->location_x--;
+	if (direction == RIGHT) head->location_x++;
+
+	set_block(head->location_x, head->location_y, BLUE);
+
+	//delete_node(head, tail->left_link);
+}
+
+void delete_node(LinkedListNode* head, LinkedListNode* removed) {
+	//if (removed == head) return;
+
+	//delete_block(removed->location_x, removed->location_y);
+
+	removed->right_link->left_link = removed->left_link;
+	removed->left_link->right_link = removed->right_link;
+
+	free(removed);
 }
