2b1ce84ad902c1a2859f639929f3483006c4d638
achieven123 <achieven123@naver.com>
2023-05-30 17:59:49 +0900

    Update

--- Winforms_Calculator/Winforms_Calculator/Form1.Designer.cs
+++ Winforms_Calculator/Winforms_Calculator/Form1.Designer.cs
@@ -28,8 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numBox = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "0";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // numBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.numBox, 4);
+            this.numBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.numBox.Font = new System.Drawing.Font("굴림", 28F, System.Drawing.FontStyle.Bold);
+            this.numBox.Location = new System.Drawing.Point(10, 10);
+            this.numBox.Margin = new System.Windows.Forms.Padding(10);
+            this.numBox.Name = "numBox";
+            this.numBox.Size = new System.Drawing.Size(311, 50);
+            this.numBox.TabIndex = 0;
+            this.numBox.Text = "0";
+            this.numBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // tableLayoutPanel1
             // 
@@ -38,6 +73,17 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.numBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 2, 5);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button7, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button8, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button9, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button10, 2, 3);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -50,25 +96,128 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(399, 362);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(331, 362);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(85, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 37);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(167, 271);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 37);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 224);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(70, 37);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "4";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(85, 224);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(70, 37);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "5";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(167, 224);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(70, 37);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "6";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(3, 177);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(70, 37);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "7";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(85, 177);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(70, 37);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "8";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(167, 177);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(70, 37);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "9";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.numpad_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(399, 362);
+            this.ClientSize = new System.Drawing.Size(331, 362);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "git";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox numBox;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
     }
 }
 
--- Winforms_Calculator/Winforms_Calculator/Form1.cs
+++ Winforms_Calculator/Winforms_Calculator/Form1.cs
@@ -12,9 +12,21 @@ namespace Winforms_Calculator
 {
     public partial class Form1 : Form
     {
+        bool isClear = true;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void numpad_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (isClear == true)
+            {
+                numBox.Text = 
+            }
         }
     }
 }
